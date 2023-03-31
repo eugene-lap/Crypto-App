e8f3c84f7eead3d76076c75dad986fab2fa25950
Eugene.Lapa <iapa2002@mail.ru>
2023-03-31 20:38:24 +0300

    init app

---
+++ crypto-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+   <div className='main-wrapper'>
+    <header className="main-header"></header>
+    <main className="main"></main>
+    <footer className="main-footer"></footer>
+   </div>
+  );
+}
+
+export default App;
