a60d006a76578465ba9903b87ce3bc5b97b26250
Eugene.Lapa <iapa2002@mail.ru>
2023-04-01 10:46:08 +0300

    project setup

--- crypto-app/src/App.tsx
+++ crypto-app/src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import './styles/config/normalize.scss'
+import './styles/config/reset.scss';
 
 function App() {
   return (
    <div className='main-wrapper'>
     <header className="main-header"></header>
-    <main className="main"></main>
+    <main className="main">
+      <button></button>
+    </main>
     <footer className="main-footer"></footer>
    </div>
   );
